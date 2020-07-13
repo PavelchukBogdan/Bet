1e445745ab9647ddf1e52c9b51de9c407c48a95a
PavelchukBogdan <pavielchuk@list.ru>
2020-07-13 17:51:11 +0300

    добавление аннотаций в репозиторий

--- src/main/java/com/bet/domain/repository/EventTypeMatchRepository.java
+++ src/main/java/com/bet/domain/repository/EventTypeMatchRepository.java
@@ -2,12 +2,15 @@ package com.bet.domain.repository;
 
 import com.bet.domain.entity.EventTypeMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
- * EventTypeMatchRepository -
+ * EventTypeMatchRepository - репозиторий свящывающий тип события и матча
  *
  * @author Павельчук Богдан (pavelchuk.b)
  * @since 10.07.2020
  */
+
+@Repository
 public interface EventTypeMatchRepository extends JpaRepository<EventTypeMatch,String> {
 }
