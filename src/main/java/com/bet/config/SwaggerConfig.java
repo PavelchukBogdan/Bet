22c624d800b108136e158027218c0bf27b7c07e1
PavelchukBogdan <pavielchuk@list.ru>
2020-07-19 16:44:07 +0300

    добавление header для проверки jwt токена

--- src/main/java/com/bet/config/SwaggerConfig.java
+++ src/main/java/com/bet/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.bet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -22,11 +27,15 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api(){
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        aParameterBuilder.name("Authorization").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        List<Parameter> aParameters = new ArrayList<>();
+        aParameters.add(aParameterBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build().globalOperationParameters(aParameters);
     }
 
 
